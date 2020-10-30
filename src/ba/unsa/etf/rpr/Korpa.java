58efd4c6820349bca0099292044dca1fa2290bfa
Samra17 <sbehic2@etf.unsa.ba>
2020-10-30 13:10:04 +0100

    Funkcionalne klase Artikl i Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl artikli[] = new Artikl[50];
+    private int brojArtikalaUKorpi = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < brojArtikalaUKorpi; i++) {
+            if(artikli[i].getKod().equals(kod)) {
+                Artikl a = artikli[i];
+                for(int j = i + 1; j < brojArtikalaUKorpi; j++) artikli[j-1] = artikli[j];
+                brojArtikalaUKorpi = brojArtikalaUKorpi - 1;
+                artikli[brojArtikalaUKorpi] = null;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int cijena = 0;
+        for(int i = 0; i < brojArtikalaUKorpi; i++) cijena = cijena + artikli[i].getCijena();
+        return cijena;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        if(brojArtikalaUKorpi == 50) return  false;
+        artikli[brojArtikalaUKorpi] = a;
+        brojArtikalaUKorpi = brojArtikalaUKorpi + 1;
+        return true;
+    }
 }
