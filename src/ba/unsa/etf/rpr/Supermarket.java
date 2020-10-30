e91d4ea2c0fea9211dfa8a40cc859f00a42c4398
Samra17 <sbehic2@etf.unsa.ba>
2020-10-30 13:15:51 +0100

    Funkcionalna i klasa Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,12 +1,29 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
-    public void dodajArtikl(Artikl biciklo) {
+
+    private Artikl supermarket[] = new Artikl[1000];
+    private  int trenutnoStanjeArtikala = 0;
+
+    public void dodajArtikl(Artikl artikl) {
+        supermarket[trenutnoStanjeArtikala] = artikl;
+        trenutnoStanjeArtikala = trenutnoStanjeArtikala + 1;
     }
 
     public Artikl[] getArtikli() {
+        return supermarket;
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < trenutnoStanjeArtikala; i++) {
+            if(supermarket[i].getKod().equals(kod)) {
+                Artikl a = supermarket[i];
+                for(int j = i+1; j < trenutnoStanjeArtikala; j++ )
+                    supermarket[j-1] = supermarket[j];
+                supermarket[trenutnoStanjeArtikala] = null;
+                return a;
+            }
+        }
+        return null;
     }
 }
